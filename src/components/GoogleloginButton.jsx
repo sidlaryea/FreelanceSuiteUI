fa66e19fe09c872c3ca9d8d77a8fd5fa711a6c67
Sidney Laryea <slaryea@ghanaports.gov.gh>
2026-07-20 02:15:22 +0000

    fix googleloginbutton path

--- src/components/GoogleloginButton.jsx
+++ src/components/GoogleloginButton.jsx
@@ -2,12 +2,12 @@ import { GoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 
 
-    const API_URL = import.meta.env.VITE_API_URL;
+    //const API_URL = import.meta.env.VITE_API_URL;
   <GoogleLogin
   onSuccess={async (credentialResponse) => {
     console.log("ID TOKEN:", credentialResponse.credential);
 
-    await axios.post(`${API_URL}/api/Login/google-login`, {
+    await axios.post(`https://invoiceapi-gcc3duhbc4age6bw.southafricanorth-01.azurewebsites.net/api/Login/google-login`, {
       idToken: credentialResponse.credential,
     });
   }}
